3014afef6e5872b299577bbfb49e7a7c0b840161
fnk0 <marcusandreog@gmail.com>
2015-10-03 23:11:32 -0500

    Initial Commit

---
+++ BEO AppTests/BEO_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  BEO_AppTests.swift
+//  BEO AppTests
+//
+//  Created by Marcus Gabilheri on 10/3/15.
+//  Copyright © 2015 Team Black. All rights reserved.
+//
+
+import XCTest
+@testable import BEO_App
+
+class BEO_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
