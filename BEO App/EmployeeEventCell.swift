1306828db07737d90245b6c1271c2a6ecfab4fca
bjesper <bjesper@mac09.cs.okstate.edu>
2015-11-05 11:42:03 -0600

    Created custom cells for event list

---
+++ BEO App/EmployeeEventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeeEventCell.swift
+//  BEO App
+//
+//  Created by JESPERSEN BENJAMIN J on 11/5/15.
+//  Copyright © 2015 Team Black. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeEventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BEO App/EmployeeTaskTableView.swift
@@ -0,0 +1,21 @@
+//
+//  EmployeeTaskTableView.swift
+//  BEO App
+//
+//  Created by JESPERSEN BENJAMIN J on 11/5/15.
+//  Copyright © 2015 Team Black. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTaskTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
